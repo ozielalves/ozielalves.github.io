9c833b810314ffb32d40ead6877103cb399368f9
Oziel Alves <ozielalves@ufrn.edu.br>
2019-01-03 21:21:11 -0300

    Update materialize.js

--- js/materialize.js
+++ js/materialize.js
@@ -6,7 +6,7 @@
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
+    var instances = M.Carousel.init(elems, onCycleTo);
   });
 
   document.addEventListener('DOMContentLoaded', function() {
